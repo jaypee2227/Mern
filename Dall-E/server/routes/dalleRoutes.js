26f8de40964206e1ea91c4c058d13686133a66a6
Jaypee2227 <bvjayprakash@gmail.com>
2023-09-15 17:29:00 +0530

    ten commit

--- Dall-E/server/routes/dalleRoutes.js
+++ Dall-E/server/routes/dalleRoutes.js
@@ -1,9 +1,19 @@
 import express from "express"
 import * as dotenv from "dotenv"
-//import {configuration, openAIApi} from "openai"
+import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 
 const router = express.Router()
+
+const configuration = new Configuration({
+    apikey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+router.route('/').get((req,res) => {
+    res.send('Hello from DALL-E!');
+})
 
 export default router;
